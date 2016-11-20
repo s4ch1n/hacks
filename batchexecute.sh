5989e43f8729af8a3d02abac8aa040d36fdfe280
sachinjose2007 <sachinjose.mec@gmail.com>
2016-11-20 14:31:53 -0500

    Comments and code refactoring.
    Added comments.

--- batchexecute.sh
+++ batchexecute.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # Batch process.
 
+################# How to execute ################# 
+# 1. Input txt file location - Input file should contain the file list.
+# 2. Update group size Line # - it specifies the number of files to be processed 
+# 3. Replace <COMMAND> with the required command. 
+###################################################
+
 # Input file 
 filename=inputfile.txt
 
@@ -12,13 +18,13 @@ end1=$group;
 for((start=1;end1<lineCount;start=start+group,end1=end1+group)) 
 do 
 	echo "$start : $end1"
-    fileToMove=`sed -n "${start},${end1}p" $filename | tr '\n' ' ' `
+    filesList=`sed -n "${start},${end1}p" $filename | tr '\n' ' ' `
 # Replace this command. 
-	hadoop fs -mv $fileToMove /user/hive/warehouse/JUNK
+	<COMMAND> ${filesList}
  done 
 
 # Replace this command. 
-fileToMove=`sed -n "${start},${end1}p" $filename | tr '\n' ' ' `
-hadoop fs -mv $fileToMove /user/hive/warehouse/JUNK
-	
+filesList=`sed -n "${start},${end1}p" $filename | tr '\n' ' ' `
+
+<COMMAND> ${filesList}
 	
